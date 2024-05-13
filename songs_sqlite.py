957a11c66fed38d0d3287a6bb4f10de993908775
Aiden Escamilla <aiden.escamilla@gmail.com>
2024-05-13 11:00:34 -0500

    Rename old database

---
+++ songs_sqlite.py
@@ -0,0 +1,101 @@
+
+import sqlite3
+import ssl
+
+class MockSongs:
+    def __init__(self) -> None:
+        pass
+
+    def insert_into_not_found(self, songNotFound):                                                    #Maybe prob here
+        pass
+
+    def insert_song(self, songDict):
+        pass
+
+class Songs:
+    def __init__(self): #
+        ssl._create_default_https_context = ssl._create_unverified_context
+        con = sqlite3.connect("Songs.db")
+        con.row_factory = sqlite3.Row
+
+        con.execute("PRAGMA foreign_keys = ON")
+        con.execute("CREATE TABLE IF NOT EXISTS Song(url PRIMARY KEY, name, artist, sentiment_Score, Category)")
+        con.execute("CREATE TABLE IF NOT EXISTS Song_Not_Found(name, artist, error, url, FOREIGN KEY(url) REFERENCES Song (url))")
+        con.execute("CREATE TABLE IF NOT EXISTS Lyrics(lyrics, url, FOREIGN KEY(url) REFERENCES Song(url))")
+        con.commit()
+        self.connection = con
+    
+    def __str__(self): #
+        result = self.connection.execute('SELECT * FROM Song INNER JOIN Lyrics on Song.url = Lyrics.url LIMIT 1')
+        row = result.fetchone()
+        return str(row['name'], ': ',  row['artist'])
+    
+    def all_songs_with_lyrics(self):
+        result = self.connection.execute('SELECT * FROM Song INNER JOIN Lyrics on Song.url = Lyrics.url WHERE length(lyrics) > 0')
+        rows = result.fetchall()
+        return rows
+    
+    def search_user_songs(self, title):
+        half_input = title[:int(len(title)/2)]
+        result = self.connection.execute('SELECT * FROM Song WHERE name LIKE ?', ['%'+title+'%'])
+        if result == None:
+            result = self.connection.execute('SELECT * FROM Song WHERE name LIKE ?',['%'+half_input+'%'])
+        rows = result.fetchone()
+        
+        if rows == None:
+            return -1
+        else:
+            return rows
+    
+    def search_user_song_and_artist(self, title, artist):
+        half_input = title[:int(len(title)/2)]
+        result = self.connection.execute('SELECT * FROM Song WHERE name LIKE ? AND artist LIKE ?', ['%'+title+'%', '%'+artist+'%'])
+        if result == None:
+            result = self.connection.execute('SELECT * FROM Song WHERE name LIKE ? AND artist LIKE ?',['%'+half_input+'%', '%'+artist+'%'])
+        rows = result.fetchone()
+
+        if rows == None:
+            return -1
+        else:
+            return rows
+
+    def find_song_with_name_artist_and_lyrics(self, title, artist):
+        result = self.connection.execute('SELECT * FROM Song INNER JOIN Lyrics on Song.url = Lyrics.url WHERE name = ? AND artist = ? AND length(lyrics) > 0', [title, artist])
+        rows = result.fetchone()
+        return rows
+    
+    def get_category_rows(self, category):
+        result = self.connection.execute('SELECT * FROM Song WHERE Category = ?', [category])
+        rows = result.fetchall()
+
+        if len(rows) == 0:
+            return -1
+        else:
+            return rows
+    
+    def get_random_rows(self):
+        result = self.connection.execute('SELECT * FROM Song ORDER BY RANDOM() LIMIT 5')
+        rows = result.fetchall()
+        return rows
+    
+    def insert_song(self, songDict):
+        result = self.connection.execute('INSERT OR REPLACE INTO Song(url, name, artist) VALUES(:url, :name, :artist)', songDict)
+        self.connection.commit()
+
+    def get_all_songs(self):
+        result = self.connection.execute('SELECT * FROM Song')
+        rows = result.fetchall()
+        return rows
+    
+    def find_song_with_name_and_artist(self, name, artist):
+        result = self.connection.execute('SELECT * FROM Song WHERE name = ? AND artist = ?', [name, artist])
+        rows = result.fetchone()
+        return rows
+    
+    def insert_into_not_found(self, song_not_found):
+        result = self.connection.execute('INSERT OR REPLACE INTO Song_Not_Found VALUES( :name, :artist, :error, :url)', song_not_found)
+        self.connection.commit()
+
+    def insert_into_lyrics(self, lyrics_and_url):                                                     #Maybe prob here
+        result = self.connection.execute('INSERT OR REPLACE INTO Lyrics VALUES(:lyrics, :url)', lyrics_and_url)
+        self.connection.commit()
