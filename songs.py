4802b4f07b2632a10490b62335f0911f671d5885
Aiden Escamilla <aiden.escamilla@gmail.com>
2024-05-13 11:00:01 -0500

    Migrated database to postgres for pgvector

--- songs.py
+++ songs.py
@@ -1,6 +1,5 @@
-
-import sqlite3
-import ssl
+import psycopg2
+import os
 
 class MockSongs:
     def __init__(self) -> None:
@@ -12,90 +11,204 @@ class MockSongs:
     def insert_song(self, songDict):
         pass
 
+
 class Songs:
+# conn = psycopg2.connect(database="johann_songs",
+#                         host="localhost",
+#                         user="postgres",
+#                         password="taako",
+#                         port="5433")
+
+# cursor = conn.cursor()
+# cursor.execute("SELECT * FROM songs")
+# print(cursor.fetchall())
+
+# conn.close()
     def __init__(self):
-        ssl._create_default_https_context = ssl._create_unverified_context
-        con = sqlite3.connect("Songs.db")
-        con.row_factory = sqlite3.Row
-
-        con.execute("PRAGMA foreign_keys = ON")
-        con.execute("CREATE TABLE IF NOT EXISTS Song(url PRIMARY KEY, name, artist, sentiment_Score, Category)")
-        con.execute("CREATE TABLE IF NOT EXISTS Song_Not_Found(name, artist, error, url, FOREIGN KEY(url) REFERENCES Song (url))")
-        con.execute("CREATE TABLE IF NOT EXISTS Lyrics(lyrics, url, FOREIGN KEY(url) REFERENCES Song(url))")
-        con.commit()
-        self.connection = con
-    
-    def __str__(self):
-        result = self.connection.execute('SELECT * FROM Song INNER JOIN Lyrics on Song.url = Lyrics.url LIMIT 1')
-        row = result.fetchone()
-        return str(row['name'], ': ',  row['artist'])
-    
-    def all_songs_with_lyrics(self):
-        result = self.connection.execute('SELECT * FROM Song INNER JOIN Lyrics on Song.url = Lyrics.url WHERE length(lyrics) > 0')
-        rows = result.fetchall()
-        return rows
-    
-    def search_user_songs(self, title):
-        half_input = title[:int(len(title)/2)]
-        result = self.connection.execute('SELECT * FROM Song WHERE name LIKE ?', ['%'+title+'%'])
-        if result == None:
-            result = self.connection.execute('SELECT * FROM Song WHERE name LIKE ?',['%'+half_input+'%'])
-        rows = result.fetchone()
+        pg_pass = os.environ['PG_PASS']
+        conn = psycopg2.connect(database="johann",
+                            host="localhost",
+                            user="aidenescamilla",
+                            password=pg_pass,
+                            port="5432")
         
-        if rows == None:
-            return -1
-        else:
-            return rows
-    
-    def search_user_song_and_artist(self, title, artist):
-        half_input = title[:int(len(title)/2)]
-        result = self.connection.execute('SELECT * FROM Song WHERE name LIKE ? AND artist LIKE ?', ['%'+title+'%', '%'+artist+'%'])
-        if result == None:
-            result = self.connection.execute('SELECT * FROM Song WHERE name LIKE ? AND artist LIKE ?',['%'+half_input+'%', '%'+artist+'%'])
-        rows = result.fetchone()
+        self.connection = conn
 
-        if rows == None:
+    def __str__(self):
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT * FROM songs INNER JOIN lyrics on songs.url = lyrics.url LIMIT 1')
+        row = cursor.fetchone()
+        # returns (songs.url, songs.name, songs.artist, songs.embeddings, lyrics.url, lyrics.lyrics)
+        return(str(row[1] + ': ' + row[2]))
+
+    def all_songs_with_lyrics(self):
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT songs.url, name, artist, embeddings, lyrics FROM songs INNER JOIN lyrics on songs.url = lyrics.url WHERE length(lyrics) > 0')
+        return cursor.fetchall()
+    
+
+    ##### Search functions
+        ##### User search functions
+
+    def search_user_song_by_title(self, title):
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT * FROM songs WHERE name LIKE %s', (title,))
+        result = cursor.fetchone()
+
+        if result == None:
+            half_input = title[:int(len(title)/2)]
+            psql_formatted_half_input = "%"+half_input+"%"
+            cursor.execute('SELECT * FROM songs WHERE name ILIKE %s',(psql_formatted_half_input,))
+            result = cursor.fetchone()
+        
+        if result == None:
             return -1
         else:
-            return rows
+            result_dict = {
+                'url' : result[0],
+                'name' : result[1],
+                'artist' : result[2]
+            }
+            return result_dict
+    
+    def search_user_song_by_title_and_artist(self, title, artist):
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT * FROM songs WHERE name ILIKE %s AND artist ILIKE %s', (title, artist,))
+        result = cursor.fetchone()
+
+        if result == None:
+            half_input = title[:int(len(title)/2)]
+            cursor.execute('SELECT * FROM songs WHERE name ILIKE %s AND artist ILIKE %s',('%'+half_input+'%', artist,))
+            result = cursor.fetchone()
+        
+
+        if result == None:  # If still no result
+            return -1
+        else:
+            result_dict = {
+                'url' : result[0],
+                'name' : result[1],
+                'artist' : result[2]
+            }
+            print("RESULTYYYY: ", result_dict)
+            return result_dict
+        
+
+        ##### Databse search functions
 
     def find_song_with_name_artist_and_lyrics(self, title, artist):
-        result = self.connection.execute('SELECT * FROM Song INNER JOIN Lyrics on Song.url = Lyrics.url WHERE name = ? AND artist = ? AND length(lyrics) > 0', [title, artist])
-        rows = result.fetchone()
-        return rows
-    
-    def get_category_rows(self, category):
-        result = self.connection.execute('SELECT * FROM Song WHERE Category = ?', [category])
-        rows = result.fetchall()
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT lyrics.lyrics, songs.artist, songs.name, songs.url \
+                        FROM songs INNER JOIN lyrics on songs.url = lyrics.url \
+                        WHERE name ILIKE %s AND artist ILIKE %s AND length(lyrics) > 0', (title, artist,))
+        result = cursor.fetchone()
 
-        if len(rows) == 0:
-            return -1
+        if len(result >= 4):
+            result_dict = {
+                    'lyrics' : result[0],
+                    'artist' : result[1],
+                    'name' : result[2],
+                    'url' : result[3]
+                }
         else:
-            return rows
-    
-    def get_random_rows(self):
-        result = self.connection.execute('SELECT * FROM Song ORDER BY RANDOM() LIMIT 5')
-        rows = result.fetchall()
-        return rows
-    
-    def insert_song(self, songDict):
-        result = self.connection.execute('INSERT OR REPLACE INTO Song(url, name, artist) VALUES(:url, :name, :artist)', songDict)
-        self.connection.commit()
+            result_dict = None
 
-    def get_all_songs(self):
-        result = self.connection.execute('SELECT * FROM Song')
-        rows = result.fetchall()
-        return rows
+        return result_dict
     
     def find_song_with_name_and_artist(self, name, artist):
-        result = self.connection.execute('SELECT * FROM Song WHERE name = ? AND artist = ?', [name, artist])
-        rows = result.fetchone()
-        return rows
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT * FROM songs WHERE name = %s AND artist = %s', (name, artist,))
+        result = cursor.fetchone()
+        return result
     
-    def insert_into_not_found(self, song_not_found):
-        result = self.connection.execute('INSERT OR REPLACE INTO Song_Not_Found VALUES( :name, :artist, :error, :url)', song_not_found)
+    #### Database fetch functions
+
+    def get_random_rows(self):
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT * FROM songs ORDER BY RANDOM() LIMIT 5')
+        result = cursor.fetchall()
+        return result
+    
+    def get_all_songs(self):
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT * FROM songs')
+        result = cursor.fetchall()
+        return result
+    
+    
+    ##### Insert functions
+
+    def insert_song(self, songDict):
+        cursor = self.connection.cursor()
+        row_info = (songDict['url'], songDict['name'], songDict['artist'])
+
+        cursor.execute('INSERT INTO songs(url, name, artist) \
+                        VALUES(%s, %s, %s) \
+                        ON CONFLICT(url) DO UPDATE SET \
+                        url = EXCLUDED.url, \
+                        name = EXCLUDED.name, \
+                        artist = EXCLUDED.artist', row_info)
+
         self.connection.commit()
 
+    ''' Takes a dict {url : url-example.com, lyrics : 'These are cool lyrics'}'''
     def insert_into_lyrics(self, lyrics_and_url):                                                     #Maybe prob here
-        result = self.connection.execute('INSERT OR REPLACE INTO Lyrics VALUES(:lyrics, :url)', lyrics_and_url)
+        cursor = self.connection.cursor()
+
+        row_info = (lyrics_and_url['url'], lyrics_and_url['lyrics'])
+        cursor.execute('INSERT INTO lyrics(url, lyrics) \
+                        VALUES(%s, %s)\
+                        ON CONFLICT(url) DO UPDATE SET\
+                        url = EXCLUDED.url, \
+                        lyrics = EXCLUDED.lyrics', row_info)
+
         self.connection.commit()
+
+    ''' Takes a dict {url : 'url-example.com', error : 'BrokenCodeError', name : song_title, artist : artist_name}'''
+    def insert_into_not_found(self, song_not_found):
+        cursor = self.connection.cursor()
+        row_info = (song_not_found['url'], song_not_found['name'], song_not_found['artist'], song_not_found['error'])
+        cursor.execute('INSERT INTO not_found(url, name, artist, error) \
+                        VALUES( %s,  %s,  %s,  %s) \
+                        ON CONFLICT(url) DO UPDATE SET \
+                        url = EXCLUDED.url, \
+                        name = EXCLUDED.name, \
+                        artist = EXCLUDED.artist, \
+                        error = EXCLUDED.error', row_info)
+        self.connection.commit()
+        print("INSERTED")
+        
+
+
+
+# if __name__ == '__main__':
+    # test = songs()
+    # test.__str__()
+    # results = test.search_user_song_by_title('show')
+    # print(results)
+    # results = test.search_user_song_by_title_and_artist('call your ', 'Noah Kahan')
+    # print(results)
+    # print(test.find_song_with_name_artist_and_lyrics('Call your mom','noah Kahan'))
+    # print(test.get_random_rows())
+    # print(test.get_all_songs())
+    # print(test.find_song_with_name_and_artist('shower song','fredo disco'))
+    # info = {
+    #     'url' : 'https://genius.com/the-backseat-lovers-growingdying-lyrics',
+    #     'name'  : 'Growing/Dying',
+    #     'artist' : 'The Backseat Lovers'
+    # }
+    # test.insert_song(info)
+
+    # test.insert_into_lyrics({ 'url' : 'https://genius.com/noah-kahan-call-your-mom-lyrics', 
+    #     'lyrics' : '''Oh, you're spiralin' again. The moment right before it ends you're most afraid of. But don't you cancel any plans. Because I won't let you get the chance to never make them. Stayed on the line with you the entire night. Til you let it out and let it in. Don't let this darkness fool you. All lights turned off can be turned on. I'll drive, I'll drive all night. I'll call your mom. Oh, dear,\
+    #         don't be discouraged. I've been exactly where you are. I'll drive, I'll drive all night. I'll call your mom. I'll call your mom. Waiting room, no place to stand. Just greatest fears and wringing hands and the loudest silence. If you could see yourself like this. If you could see yourself like this, you'd've never tried it. Stayed on the line with you the entire night. Til you told me that you had to go. \
+    #         Don't let this darkness fool you. All lights turned off can be turned on. I'll drive, I'll drive all night. I'll call your mom. Oh, dear, don't be discouraged. I've been exactly where you are. I'll drive, I'll drive all night. I'll call your mom. Medicate, meditate, swear your soul to Jesus. Throw a punch, fall in love, give yourself a reason. Don't want to drive another mile wonderin' if you're breathin. So won't you stay, won't you stay, won't you stay with me?.\
+    #         Medicate, meditate, save your soul for Jesus. Throw a punch, fall in love, give yourself a reason. Don't want to drive another mile without knowin' you're breathin. So won't you stay, won't you stay, won't you stay with me?. Don't let this darkness fool you. All lights turned off can be turned on. I'll drive, I'll drive all night. I'll call your mom. Oh, dear, don't be discouraged. \
+    #         I've been exactly where you are. I'll drive, I'll drive all night. I'll call your mom. I'll call your mom'''})
+    # not_found_test = {
+    #     'url' : 'https://genius.com/the-backseat-lovers-growingdying-lyrics',
+    #     'name' : 'Growing/Dying',
+    #     'artist' : 'The Backseat Lovers',
+    #     'error' : 'urllib.error.HTTPError'
+    # }
+    # test.insert_into_not_found(not_found_test)
